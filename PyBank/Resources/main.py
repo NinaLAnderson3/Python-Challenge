3d6fd06a8e696acc314aed5f3e5027b0b4438668
NinaAnderson <ninamonaco@yahoo.com>
2020-11-16 10:09:57 -0500

    variables

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,8 +1,15 @@
 import os
 import csv
 
+#define variables
+month_count = 0
+months = []
+total_rev = 0
+current_rev = 0
+last_rev = 0
+rev_change = 0
+changes = []
 budgetData_csv = os.path.join('budget_data.csv')
 
 with os.path.join('budget_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    
