1c2ded3fb318de9f756fd2f03d58c7c93d6d9521
NinaAnderson <ninamonaco@yahoo.com>
2020-11-16 11:28:48 -0500

    next go

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -3,8 +3,17 @@ import csv
 
 fhand = open('budget_data.csv')
 csv_f = csv.reader(fhand)
+fields = next(csv_f)
 months = []
+net_change = []
+profit = []
 
 for row in csv_f:
     months.append(row[0])
-    print(len(months)-1)
+    profit.append(int(row[1]))
+
+for i in range(len(profit)-1):
+    net_change.append(profit[i+1]-profit[i])
+    #print(months + net_change)
+print(fields)
+print(len(months))
