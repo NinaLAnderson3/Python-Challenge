d7d2357bedcb79b63075b811da3dd0a9f29e2cbd
NinaAnderson <ninamonaco@yahoo.com>
2020-11-16 08:39:02 -0500

    try again

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -2,3 +2,7 @@ import os
 import csv
 
 budgetData_csv = os.path.join('budget_data.csv')
+
+with os.path.join('budget_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
