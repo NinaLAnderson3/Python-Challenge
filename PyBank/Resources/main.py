29312889421593691b8e052f41c7626b57f51a51
NinaAnderson <ninamonaco@yahoo.com>
2020-11-16 10:14:49 -0500

    more stuff

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -13,3 +13,6 @@ budgetData_csv = os.path.join('budget_data.csv')
 
 with os.path.join('budget_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    print(header)
