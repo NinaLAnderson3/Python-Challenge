21de7cd5121a9c9878386b29be7f68d59322c589
NinaAnderson <ninamonaco@yahoo.com>
2020-11-16 13:45:39 -0500

    corrected traceback

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -4,42 +4,42 @@ import csv
 budget_data = os.path.join('budget_data.csv')
 
 total_months = 0
-total_pl = 0
+profit_loss = 0
 value = 0
-dates = []
+months = []
 profits = []
 
 with open(budget_data, newline='',) as csvfile:
     csvreader = csv.reader(csvfile, delimiter =',')
     csv_header = next(csvreader)
     first_row = next(csvreader)
-    total_months +=2
-    total_pl += int(first_row[1])
+    total_months +=1
+    profit_loss += int(first_row[1])
     value = int(first_row[1])
 
     for row in csvreader:
-        dates.append(row[0])
+        months.append(row[0])
         change = int(row[1])-value
         profits.append(change)
         value = int(row[1])
 
         #total_months += 1
-        total_pl = total_pl+int(row[1])
+        profit_loss = profit_loss+int(row[1])
 
         greatest_increase = max(profits)
-        greatest_index = profits.index(greatest_increase)
-        best_date = dates[greatest_index]
+        gain = profits.index(greatest_increase)
+        best_date = months[gain]
 
         max_decrease = min(profits)
-        worst_index = profits.index(max_decrease)
-        worst_date = dates[worst_index]
+        loss = profits.index(max_decrease)
+        worst_date = months[loss]
 
         avg_change = sum(profits)/len(profits)
 
 
-print("Total months: ", len(dates))
+print("Total months: ", len(months)+1)
 print("Average Change: $", avg_change)
-print("Total is:", total_pl)
+print("Total is:", profit_loss)
 print("Greatest Increase: ", best_date, greatest_increase)
 print("Greatest Decrease: ", worst_date, max_decrease)
 
@@ -48,7 +48,7 @@ output = open("analysis.txt","w")
 line1 = "Financial Analysis"
 line2 = "----------------"
 line3 = str(f"Total Months: {str(total_months)}")
-line4 = str(f"Total: ${str(total_pl)}")
+line4 = str(f"Total: ${str(profit_loss)}")
 line5 = str(f"Average Change: ${str(round(avg_change))}")
 line6 = str(f"Greatest Increase in Profits: {best_date}${str(greatest_increase)}")
 line7 = str(f"Greatest decrease in profits: {worst_date}(${str(max_decrease)})")
