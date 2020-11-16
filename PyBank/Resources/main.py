01131f6b51676bd3c63c81e684abf67f132d941c
NinaAnderson <ninamonaco@yahoo.com>
2020-11-16 11:48:50 -0500

    changes

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -20,8 +20,9 @@ for i in range(len(profit)-1):
 
     max_decrease = min(net_change)
     Min_date = str(months[net_change.index(min(net_change))])
-    #print(months + net_change)
-print(Max_date, max_increase)
-print(Min_date, max_decrease)
-print(fields)
-print(len(months))
+
+
+print("Total months: ", len(months))
+print("Average Change: $", average_change)
+print("Greatest Increase: ", Max_date, max_increase)
+print("Greatest Decrease: ", Min_date, max_decrease)
