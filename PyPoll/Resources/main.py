c538ed33e02443966a00dfd73d450e2614fecae0
NinaAnderson <ninamonaco@yahoo.com>
2020-11-17 15:56:40 -0500

    percent code

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -22,7 +22,17 @@ with open(election_data, newline="") as csvfile:
         else:
             index = candidates.index(row[2])
             num_votes[index] += 1
-print(candidates, num_votes)
+#print(candidates, num_votes)
+    for votes in num_votes:
+        percent = round((votes/total_votes) * 100,2)
+        percent_votes.append(percent)
+    #print(percent_votes)
+
+    winner = max(num_votes)
+    index = num_votes.index(winner)
+    winning_candidate = candidates[index]
+
+    print(winning_candidate, num_votes[0])
 
 
 #print("Election Results")
