71828dfb96d7e68309453598f8041a2174b9854f
NinaAnderson <ninamonaco@yahoo.com>
2020-11-17 15:48:44 -0500

    pypoll code

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -1,32 +1,36 @@
 import csv
 import os
+
 election_data = os.path.join("election_data.csv")
 
 candidates = []
-total_votes = []
-vote_counter = 0
-perc_vote = []
+num_votes = []
+percent_votes = []
+total_votes = 0
 
 with open(election_data, newline="") as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=",")
-    csvheader = next(csvreader)
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
 
     for row in csvreader:
-        vote_counter = vote_counter + 1
-#create list of all candidates
+        total_votes += 1
+
         if row[2] not in candidates:
             candidates.append(row[2])
+            index = candidates.index(row[2])
+            num_votes.append(1)
         else:
-            continue
-    print(candidates)
+            index = candidates.index(row[2])
+            num_votes[index] += 1
+print(candidates, num_votes)
 
 
-print("Election Results")
-print("------------------")
-print(f"Total Votes: {str(total_votes)}")
-print("------------------")
-for x in range(len(candidates)):
-    print(f"{candidates[i]}: {str(perc_vote[i])} (str{str(num_votes[i])})")
-print("-----------------")
-print(f"Winner: {winner_name}")
-print("--------------------")
+#print("Election Results")
+#print("------------------")
+#print(f"Total Votes: {str(total_votes)}")
+#print("------------------")
+#for x in range(len(candidates)):
+#    print(f"{candidates[i]}: {str(perc_vote[i])} (str{str(num_votes[i])})")
+#print("-----------------")
+#print(f"Winner: {winner_name}")
+#print("--------------------")
